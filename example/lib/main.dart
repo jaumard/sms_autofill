4648f3d25517ba06eadf91116b6beae7371c5432
Pedro Massango <pedro.massango@nextbss.co.ao>
2020-04-02 10:13:37 +0100

    Added a new page to test the listening for code vis CodeAutoFill mixin

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -96,10 +96,89 @@ class _HomePageState extends State<HomePage> {
                   setState((){});
                 },
               ),
-
+              RaisedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (_) => CodeAutoFillTestPage()
+                    )
+                  );
+                },
+                child: Text("Test CodeAutoFill mixin"),
+              )
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class CodeAutoFillTestPage extends StatefulWidget {
+  @override
+  _CodeAutoFillTestPageState createState() => _CodeAutoFillTestPageState();
+}
+
+class _CodeAutoFillTestPageState extends State<CodeAutoFillTestPage>
+  with CodeAutoFill {
+
+  String appSignature;
+  String otpCode;
+
+  @override
+  void codeUpdated() {
+    setState(() {
+      otpCode = code;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    listenForCode();
+
+    SmsAutoFill().getAppSignature.then((signature) {
+      setState(() {
+        appSignature = signature;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    cancel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(fontSize: 18);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Listening for code"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(32, 32, 32, 0),
+            child: Text("This is the current app signature: $appSignature",),
+          ),
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32),
+            child: Builder(
+              builder: (_) {
+                if (otpCode == null) {
+                  return Text("Listening for code...", style: textStyle);
+                }
+                return Text("Code Received: $otpCode", style: textStyle);
+              },
+            ),
+          ),
+          const Spacer(),
+        ],
       ),
     );
   }
