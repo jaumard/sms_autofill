52cd571b2a05efa34d1954098addc849c5662ba0
a.akbarzadeh <a.akbarzadeh>
2026-09-02 09:25:12 +0330

    feature: migrating the UI to standalone (material_ui)

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:material_ui/material_ui.dart';
 import 'package:sms_autofill/sms_autofill.dart';
 
 void main() => runApp(const MyApp());
--- lib/sms_autofill.dart
+++ lib/sms_autofill.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
+import 'package:material_ui/material_ui.dart';
 import 'package:flutter/services.dart';
 import 'package:pin_input_text_field/pin_input_text_field.dart';
 
